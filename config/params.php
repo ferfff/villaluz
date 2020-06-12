42b616b52aaa9f572bfa0ef59175d0fec7746b35
Fernando Maldonado <ferchofff@gmail.com>
2020-06-11 21:55:50 -0500

    Cambiando bootstrap a 4

--- config/params.php
+++ config/params.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'bsVersion' => '4.x',
+    'adminEmail' => 'contacto@villaluz.com',
+    'senderEmail' => 'contacto@villaluz.com',
+    'senderName' => 'Villaluz',
 ];
